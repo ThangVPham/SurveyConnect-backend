45347b5765c37fff747b3fba7262808c881bc5f7
Thang Pham <thangpham1412@gmail.com>
2023-11-05 11:11:05 -0500

    changed ping interval

--- src/app.ts
+++ src/app.ts
@@ -9,6 +9,18 @@ import UserRoutes from "./Routes/UserRoutes.ts";
 import moment from "moment";
 import axios from "axios";
 
+const survey_url = "https://surveyconnect-backend.onrender.com/api/surveys";
+const id = setInterval(() => {
+  WakeServer();
+}, 840000);
+async function WakeServer() {
+  try {
+    const survey_res = await axios.get(survey_url);
+    console.log(`${moment().format("MMMM Do YYYY, h:mm:ss a")} - Survey Connect Pinged`);
+  } catch (e) {
+    console.log(`${moment().format("MMMM Do YYYY, h:mm:ss a")} - ${e}`);
+  }
+}
 const app = express();
 app.use(express.json());
 app.use(
@@ -33,15 +45,3 @@ app.use("/", (req: express.Request, res: express.Response) => {
       "Thanks for checking us out. Please make sure you're using the correct API endpoints when requesting data.",
   });
 });
-const survey_url = "https://surveyconnect-backend.onrender.com/api/surveys";
-const id = setInterval(() => {
-  WakeServer();
-}, 840000);
-async function WakeServer() {
-  try {
-    const survey_res = await axios.get(survey_url);
-    console.log(`${moment().format("MMMM Do YYYY, h:mm:ss a")} - Survey Connect Pinged`);
-  } catch (e) {
-    console.log(`${moment().format("MMMM Do YYYY, h:mm:ss a")} - ${e}`);
-  }
-}
